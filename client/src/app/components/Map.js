dc5789d7dc6cde3c9297aff6f0ff1c71e86f64bc
Andy Kim <26eckim@wonhago.org>
2024-10-19 11:03:22 +0900

    kakao map updates

--- client/src/app/components/Map.js
+++ client/src/app/components/Map.js
@@ -1,27 +1,30 @@
-'use client';
-import { useEffect, useRef } from 'react';
-import Script from 'next/script';
- 
-export default function Map() {
+import Script from 'next/script'
+import {React, useRef, useEffect} from 'react'
 
-  
-  const mapRef = useRef<HTMLDivElement>(null);
-  useEffect(() => {
+function Map() {
+
+  const mapRef = useRef(null)
+
+  useEffect(()=>{
     const options = {
-      //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-      level: 3, //지도의 레벨(확대, 축소 정도)
-    };
- 
-    const map = new kakao.maps.Map(mapRef, options); //지도 생성 및 객체 리턴
-  }, []);
- 
+      center : new window.kakao.maps.LatLng(3,3),
+      level : 3
+    }
+
+    const map = new window.kakao.maps.Map(mapRef, options)
+  },[])
+
+
+
+
+
   return (
-    <>
+    <div>
       <Script
-        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.ca0517e212eef9e2b05b7cb7b27e8bb4}`}
-      />
-      <div ref={mapRef} className=""></div>;
-    </>
-  );
+        src={"//dapi.kakao.com/v2/maps/sdk.js?appkey=ca0517e212eef9e2b05b7cb7b27e8bb4"}
+      ></Script>
+    </div>
+  )
 }
+
+export default Map
