a81ef98c0b61662a426911d0427eae34bfc49ad8
Andy Kim <26eckim@wonhago.org>
2024-10-22 00:00:35 +0900

    new map side bar UI

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -13,6 +13,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <script src="//dapi.kakao.com/v2/maps/sdk.js?appkey=ca0517e212eef9e2b05b7cb7b27e8bb4"></script>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet"></link>
 
       </head>
       <body className={poppins.className}>
