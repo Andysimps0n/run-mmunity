2bdb2d738b04c5cb4e719c0a91d30ac66b528281
Andy Kim <26eckim@wonhago.org>
2024-11-18 19:14:18 +0900

    map initial setups and layout

--- client/src/app/join/page.js
+++ client/src/app/join/page.js
@@ -1,13 +1,34 @@
-import React from 'react'
+"use client"
+
+import Script from 'next/script';
+import {React, useEffect, useRef} from 'react'
 import Header from '../components/Header'
 
 function page() {
+
+  let mapOption;
+  let map;
+
+  const kakaoMap = useRef(null);
+  const onScriptLoad = ()=>{
+    // mapOption = { 
+    //   center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+    //   level: 3 // 지도의 확대 레벨
+    // };
+    // map = new kakao.maps.Map(kakaoMap, mapOption)
+    console.log('this is onScriptLoad ')
+    // console.log(`map : ${map}`)
+  }
+
+  
   return (
-    <>
+    <div className="map-outermost-container">
+      <Script  onLoad={()=>{onScriptLoad()}} src="https://dapi.kakao.com/v2/maps/sdk.js?appkey=ca0517e212eef9e2b05b7cb7b27e8bb4"></Script>
       <Header></Header>
-      <div className="join-outer-most-contianer"></div>
+      <div ref={kakaoMap} className='kakaoMap'></div>
+
+    </div>
     
-    </>
   )
 }
 
--- client/src/layout.js
+++ client/src/layout.js
@@ -11,10 +11,6 @@ const poppins = Poppins({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-
-        <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=ca0517e212eef9e2b05b7cb7b27e8bb4"></script>
-      </head>
       <body className={poppins.className}>
         <>
         {children}
